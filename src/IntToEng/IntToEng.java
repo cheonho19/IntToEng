dbfd206ed3b4e547aea65813d6d56b2b75d30ade
yanaka_0206 <g17963yr@gm.tsuda.ac.jp>
2019-05-23 22:30:40 +0900

    4回目
    ドライバー　谷中(yana000000)
    
    ナビゲーター　金(cheonho19)

--- src/IntToEng/IntToEng.java
+++ src/IntToEng/IntToEng.java
@@ -3,7 +3,7 @@ package IntToEng;
 import java.util.Scanner;
 
 public class IntToEng {
-	
+
 static String zero2nineteen[] = {"zero","one","two","three","four","five","six","seven","eight","nine","ten",
 		         "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
 static String twenty2ninety[] = {"twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
@@ -19,6 +19,37 @@ static String thousand = "thousand";
         sc.close();
     }
     static String translateEng(int n) {
-    	   return "";
+    	String s = "";
+    	if(n < 100) {
+    		s = underHundred(n);
+    	} else if(n < 1000) {
+    		s = underThousand(n);
+    	} else if(n < 10000){
+    		s = underTenThousand(n);
+    	} else if(n == 10000) {
+    		s = "ten thousand";
+    	} else {
+    		s = "10000未満の整数を入力してください";
+    	}
+    	return s;
+    }
+    static String underHundred(int n) {
+    	String s = "";
+    	if(n < 20) {
+    		s = zero2nineteen[n];
+    	} else if(n < 100) {
+    		int i = n/10 - 2;
+			int j = n%10;
+			if(j == 0) {
+    			s = twenty2ninety[i];
+    		} else {
+    			s = twenty2ninety[i] + " " + zero2nineteen[j];
+    		}
+    	}
+    	return s;
+    }
+    static String underThousand(int n) {
+    }
+    static String underTenThousand(int n) {
     }
 }
--- test/IntToEng/IntToEngTest.java
+++ test/IntToEng/IntToEngTest.java
@@ -8,5 +8,33 @@ public class IntToEngTest {
 
 	@Test
 	public void ゼロから100未満() {
+		IntToEng ite = new IntToEng();
+		String expected = "ninety nine";
+		String actual = ite.underHundred(99);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void ゼロから1000未満() {
+		IntToEng ite = new IntToEng();
+		String expected = "nine hundred ninety nine";
+		String actual = ite.underThousand(999);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void ゼロから10000未満() {
+		IntToEng ite = new IntToEng();
+		String expected = "nine thousand nine hundred ninety nine";
+		String actual = ite.underTenThousand(9999);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void ゼロから10000まで() {
+		IntToEng ite = new IntToEng();
+		String expected1 = "ten thousand";
+		String actual1 = ite.translateEng(10000);
+		String expected2 = "10000未満の整数を入力してください！";
+		String actual2 = ite.translateEng(10001);
+		assertThat(actual1, is(expected1));
+		assertThat(actual2, is(expected2));
 	}
 }
