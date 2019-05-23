29e2acf5c9934cd8ff06223a81b4ff30ed7c589a
yanaka_0206 <g17963yr@gm.tsuda.ac.jp>
2019-05-23 22:00:57 +0900

    2回目　ドライバー 谷中(yana000000)
    ナビゲーター 金(cheonho19)
    
    testにゼロから1000未満のメソッドを追加した。

--- test/IntToEng/IntToEngTest.java
+++ test/IntToEng/IntToEngTest.java
@@ -8,5 +8,16 @@ public class IntToEngTest {
 
 	@Test
 	public void ゼロから100未満() {
+		IntToEng ite = new IntToEng();
+		String expected = "ninety nine";
+		String actual = ite.underHundred(99);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void ゼロから1000未満() {
+		IntToEng ite = new IntToEng();
+		String expected = "nine hundred ninety nine";
+		String actual = ite.underThousand(999);
+		assertThat(actual, is(expected));
 	}
 }
